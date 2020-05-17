453a03ff479c6b307f694d86cf81958aa3d061c0
daminisalwe1234 <salwedamini2gmail.com>
2020-05-17 22:45:25 +0530

    total emp Wage by companyuc9

--- EmpWageBuilderuc1.java
+++ EmpWageBuilderuc1.java
@@ -1,10 +1,21 @@
 public class EmpWageBuilderuc1 {
 	public static final int IS_PART_TIME = 1;
         public static final int IS_FULL_TIME = 2;
-        
-        public static int computeEmpWage(String company, int empRatePerHour,int numOfWorkingDays,int maxHourPerMonth) {
-                int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-                while (totalEmpHrs <= maxHourPerMonth &&
+	private final String company;
+	private final int empRatePerHour;
+	private final int numOfWorkingDays;
+	private final int maxHoursPerMonth;
+	private int totalEmpWage;
+        public EmpWageBuilderuc1(String company, int empRatePerHour,int numOfWorkingDays,int maxHourPerMonth) {
+                this.company = company;
+		this.empRatePerHour = empRatePerHour;
+		this.numofWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+	}
+
+	public void  computeEmpWage() {
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+                while (totalEmpHrs <= maxHoursPerMonth &&
                         totalWorkingDays < numOfWorkingDays) {
                         totalWorkingDays++;
                         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
@@ -21,14 +32,23 @@ public class EmpWageBuilderuc1 {
                         totalEmpHrs += empHrs;
                         System.out.println("Day#: " +totalWorkingDays + " Emp Hr: " +empHrs);
                 }
-                int totalEmpWage = totalEmpHrs * empRatePerHour;
-                System.out.println("total Emp Wage for compant: " +company+" is: " +totalEmpWage);
-                return totalEmpWage;
+                totalEmpWage = totalEmpHrs * empRatePerHour;
+                
+                
         }
+	@Override
+	public String toString() {
+		return *Total Emp Wage for Company: " +company+ " is: " + totalEmpWage;
+	}
+      
         public static void main(String[] args) {
-                computeEmpWage("dmart", 20, 2, 10);
-		computeEmpWage("mahindra", 10, 4, 20);
-
+                EmpWageBuilderuc1 dMart = new EmpWageBuilderuc1("dmart", 20, 2, 10);
+		EmpWageBuilderuc1 reliance = new EmpWageBuilderuc1("Reliance", 10, 4, 20);
+		dMart.computeEmpWage();
+		System.out.println(dMart);
+		reliance.computeEmpWage();
+		System.out.println(reliance);
         }
 }
+
 
