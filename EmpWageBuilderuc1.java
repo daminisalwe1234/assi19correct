2811ff0c92406e0452e31d51df0bd815fe31a878
daminisalwe1234 <salwedamini2gmail.com>
2020-05-17 15:32:13 +0530

    emp wage using case uc5

--- EmpWageBuilderuc1.java
+++ EmpWageBuilderuc1.java
@@ -1,17 +1,29 @@
-public class EmpWageBuilderuc1 {
-	public static void main(String[] args){
-		int IS_PART_TIME = 1;
-		int IS_FULL_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
-		int empHrs = 0;
-		int empWage = 0;
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if (empCheck == IS_PART_TIME)
-			empHrs = 8;
-		else if (empCheck == IS_PART_TIME)
-			empHrs = 4;
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("emp wage:" +empWage);
+public class EmpWageBuilderuc1  {
+	public class empWageCase {
+		void empSalary() {
+		int isPartTime = 2;
+		int empRatePrHr = 20;
+		int empr = 0;
+		int perDaySalary = 0;
+		int monthWorkingDay = 20;
+		int monthlySalary = 0;
+		for (int i = 1;i <=monthWorkingDay;i++)
+		{
+			double empCheck = Math.floor(Math.random() *10) % 2;
+			switch ((int)empCheck)
+			{
+				case isFullTime :
+					empHr = 8;
+					break;
+				case isFullTime :
+                        		empHr = 4;
+                        		break;
+			}
+			perDaySalary = empRateprHr * empHr;
+			monthlySalary = perDaySalary +montlySalary;
+		}
+		System.out.println("monthly Salary "+monthlySalary);
+
+		
 	}
 }
-
